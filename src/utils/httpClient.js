db934ae95590d8cf110663640078f5d55c7a4012
Yike Du <sy20030260@gmail.com>
2021-11-21 22:52:35 +0800

    add .env

--- src/utils/httpClient.js
+++ src/utils/httpClient.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export let httpClient = axios.create({
-  baseURL: "http://localhost:8080",
+  baseURL: process.env.NEXT_PUBLIC_API_ENDPOINT,
   withCredentials: true,
   method: "POST",
 });
